30c07087140251b19df0f34666d90c1967375602
Christopher <craftplays10a@gmail.com>
2026-08-05 15:45:37 -0300

    feature: rotas de acesso

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const cookieParser = require('cookie-parser');
-const authRoutes = require("./modules/auth/auth.routes");
+const authRoutes = require('./modules/auth/auth.routes');
+const accessRoutes = require('./modules/access/access.routes');
 
 const app = express();
 
 app.use(express.json({ limit: "100kb",}));
 app.use(cookieParser());
 
-
-app.use("/api/admin/auth", authRoutes);
+app.use('/api/admin/auth', authRoutes);
+app.use('/api/admin/access', accessRoutes);
 
 module.exports = app;
--- src/modules/access/access.routes.js
+++ src/modules/access/access.routes.js
@@ -1,2 +1,9 @@
 const express = require('express');
+const { requireAdminAuth } = require('../auth/auth.middleware');
+const { getMyAccess } = require('./access.controller');
 
+const router = express.Router();
+
+router.get('/me', requireAdminAuth, getMyAccess);
+
+module.exports = router;
