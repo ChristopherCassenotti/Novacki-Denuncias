06a728df22685ed12320300dd892c362beb0ec14
Christopher <craftplays10a@gmail.com>
2026-08-14 13:43:13 -0300

    feature: finalizado modulo de Notas para admin

--- src/modules/adminReports/admin.routes.js
+++ src/modules/adminReports/admin.routes.js
@@ -4,6 +4,7 @@ const { requireAdminAuth } = require('../auth/auth.middleware')
 const { requirePermissions } = require('../access/access.middleware');
 const { getReportHandler, getReportsHandler, updateStatusHandler, updatePriorityHandler, assignReportHandler , unassignReportHandler } = require('./admin.controller');
 const { listMessagesHandler, createMessageHandler } = require('../adminReportMessages/adminReportMessages.controller');
+const { listInternalNotesHandler, createInternalNoteHandler } = require('../adminReportsInternalNotes/adminReportInternalNotes.controller');
 
 router.use(requireAdminAuth);
 router.use(requirePermissions('REPORT_VIEW'));
@@ -11,8 +12,10 @@ router.use(requirePermissions('REPORT_VIEW'));
 router.get('/', getReportsHandler);
 router.get('/:id', getReportHandler);
 router.get('/:id/messages', requirePermissions('REPORT_MESSAGE'), listMessagesHandler);
+router.get('/:id/internal-notes', requirePermissions('REPORT_INTERNAL_NOTE'), listInternalNotesHandler);
 
 router.post('/:id/messages', requirePermissions('REPORT_MESSAGE'), createMessageHandler);
+router.post('/:id/internal-notes', requirePermissions('REPORT_INTERNAL_NOTE'), createInternalNoteHandler);
 
 router.patch('/:id/status', requirePermissions('REPORT_MANAGE'), updateStatusHandler);
 router.patch('/:id/priority', requirePermissions('REPORT_MANAGE'), updatePriorityHandler);
